56f81b03255ff5e4a5a8fab6f1e58e5362f51693
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-01-14 18:18:27 +0900

    Add ChartData

---
+++ lib/chart_data.rb
@@ -0,0 +1,42 @@
+class ChartData
+  def initialize(params = {})
+    raise 'No chart specified' unless params[:chart]
+    raise 'No stats specified' unless params[:stats]
+    raise 'No interval specified' unless params[:interval]
+
+    @chart    = params[:chart]
+    @stats    = params[:stats]
+    @element  = params[:element] || 'chart'
+    @interval = params[:interval]
+  end
+
+  def to_hash
+    @hash ||= {
+      element: @element,
+      data: data,
+      xkey: 'date',
+      ykeys: ['c'],
+      labels: [@chart.name]
+    }
+  end
+
+  def to_json
+    @json ||= to_hash.to_json
+  end
+
+  def data
+    @data ||= @stats.map { |s|
+      {
+        time: s.time.strftime(time_format),
+        c: s.count
+      }
+    }
+  end
+
+  def time_format
+    case @interval
+    when 'daily'
+      then '%Y-%m-%d'
+    end
+  end
+end
---
+++ spec/lib/chart_data_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe 'ChartData' do
+  context 'when daily interval' do
+    before do
+      @chart = Chart.new(name: 'example_chart')
+      @stats = [
+        Stat.new(time: DateTime.new(2013, 1, 1), count: 1000),
+        Stat.new(time: DateTime.new(2013, 1, 2), count: 1200),
+        Stat.new(time: DateTime.new(2013, 1, 3), count: 1500),
+      ]
+      @interval = 'daily'
+    end
+
+    subject do
+      ChartData.new(
+        chart: @chart,
+        stats: @stats,
+        interval: @interval,
+      ).to_hash
+    end
+
+    it 'should be correct hash' do
+      should eq({
+        element: 'chart',
+        data: [
+          {time: '2013-01-01', c: 1000},
+          {time: '2013-01-02', c: 1200},
+          {time: '2013-01-03', c: 1500},
+        ],
+        xkey: 'date',
+        ykeys: ['c'],
+        labels: ['example_chart'],
+      })
+    end
+  end
+end
