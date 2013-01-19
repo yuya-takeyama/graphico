e2e5c304a3f0e9ef8df79f2da87763d88974cca8
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-01-19 12:56:04 +0900

    Setup spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,29 @@
 PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
-require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 
-RSpec.configure do |conf|
-  conf.mock_with :rr
-  conf.include Rack::Test::Methods
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
+  require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+
+  RSpec.configure do |conf|
+    conf.mock_with :rr
+    conf.include Rack::Test::Methods
+  end
+
+  def app
+    ##
+    # You can handle all padrino applications using instead:
+    #   Padrino.application
+    Graphico.tap { |app|  }
+  end
 end
 
-def app
-  ##
-  # You can handle all padrino applications using instead:
-  #   Padrino.application
-  Graphico.tap { |app|  }
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
 end
