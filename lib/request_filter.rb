d3d91a8f425827c4b74235aa7c9572adcd40b566
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-03-31 19:52:38 +0900

    Add default_interval for RequestFilter

--- lib/request_filter.rb
+++ lib/request_filter.rb
@@ -8,6 +8,18 @@ class RequestFilter
       filtered_params['type'] = 'countable' unless params['type']
     end
 
+    unless filtered_params['default_interval']
+      if filtered_params['type'] == 'gauge'
+        if filtered_params[:time] =~ /^\d{4}-\d{2}-\d{2}$/
+          filtered_params['default_interval'] = 'daily'
+        elsif filtered_params[:time] =~ /^\d{4}-\d{2}$/
+          filtered_params['default_interval'] = 'monthly'
+        end
+      else
+        filtered_params['default_interval'] = filtered_params[:interval]
+      end
+    end
+
     filtered_params
   end
 end
--- spec/lib/request_filter_spec.rb
+++ spec/lib/request_filter_spec.rb
@@ -2,31 +2,64 @@ require 'spec_helper'
 
 describe RequestFilter do
   describe '#filter' do
-    subject { RequestFilter.new.filter(params) }
+    let(:filtered_params) { RequestFilter.new.filter(params) }
 
-    context 'when interval is "momentary" and type is not specified' do
-      let(:params) { {interval: 'momentary'} }
+    describe "params['type']" do
+      subject { filtered_params['type'] }
 
-      its(['type']) { should == 'gauge' }
-    end
+      context 'when interval is "momentary" and type is not specified' do
+        let(:params) { {interval: 'momentary'} }
 
-    context 'when interval is not "momentary"' do
-      ['daily', 'monthly'].each do |interval|
-        context 'type is specified' do
-          context "interval is \"#{interval}\"" do
-            let(:params) { {interval: interval, 'type' => 'uncountable'} }
+        it { should == 'gauge' }
+      end
 
-            it 'type should not be changed' do
-              should == params
+      context 'when interval is not "momentary"' do
+        ['daily', 'monthly'].each do |interval|
+          context 'type is specified' do
+            context "interval is \"#{interval}\"" do
+              let(:params) { {interval: interval, 'type' => 'uncountable'} }
+
+              it 'type should not be changed' do
+                should == 'uncountable'
+              end
+            end
+          end
+
+          context 'type is not specified' do
+            context "interval is \"#{interval}\"" do
+              let(:params) { {interval: interval} }
+
+              it { should == 'countable' }
             end
           end
         end
+      end
+    end
 
-        context 'type is not specified' do
-          context "interval is \"#{interval}\"" do
-            let(:params) { {interval: interval} }
+    describe "params['default_interval']" do
+      subject { filtered_params['default_interval'] }
 
-            its(['type']) { should == 'countable' }
+      context 'type is gauge' do
+        [
+          {time: '2013-01-01', expected: 'daily'},
+          {time: '2013-01', expected: 'monthly'},
+        ].each do |input|
+          context "time = #{input[:time]}" do
+            let(:params) { {'type' => 'gauge', time: input[:time]} }
+
+            it { should == input[:expected] }
+          end
+        end
+      end
+
+      context 'type is not gauge' do
+        ['countable', 'uncountable'].each do |type|
+          context "type is #{type}" do
+            let(:params) { {'type' => type, interval: 'daily'} }
+
+            it 'should be specified interval' do
+              should == 'daily'
+            end
           end
         end
       end
