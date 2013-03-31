5594a7dfc6219d43113b9845501fb02dd66033ad
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-03-31 17:42:55 +0900

    Add momentary interval

--- lib/request_validator.rb
+++ lib/request_validator.rb
@@ -4,7 +4,7 @@ class RequestValidator
   def validate(params)
     interval, time = params[:interval], params[:time]
 
-    unless ['daily', 'monthly'].include? interval
+    unless ['momentary', 'daily', 'monthly'].include? interval
       @message = 'Invalid interval is specified'
 
       return false
@@ -21,11 +21,15 @@ class RequestValidator
 
   private
   def correct_time_for?(interval, time)
-    case interval
+    res = case interval
+    when 'momentary' then
+      time =~ /^\d{4}(?:-\d{2}(?:-\d{2}(?: \d{2}(?::\d{2}(?::\d{2})?)?)?)?)?$/
     when 'daily' then
       time =~ /^\d{4}-\d{2}-\d{2}$/
     when 'monthly' then
       time =~ /^\d{4}-\d{2}$/
     end
+
+    res == 0
   end
 end
